e57b2f9bf73f2d43698fc06845d089e544279db9
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-02-04 17:41:13 +0530

    first commit

---
+++ mern-auth/frontend/src/slices/apiSlice.js
@@ -0,0 +1,14 @@
+import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+
+// const baseQuery = fetchBaseQuery({ baseUrl: 'import.meta.env.VITE_API_URL' });
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_URL || 'http://72.61.229.119:5000',
+  credentials: 'include',
+});
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ['User'],
+  endpoints: (builder) => ({}),
+});
